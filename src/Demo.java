dc7a3f72a59173598b703527b1d994bf1c0e32f4
Savar Jain <1723749@StamfordPS.org>
2021-10-01 12:13:01 -0400

    Added 1==1

--- src/Demo.java
+++ src/Demo.java
@@ -2,5 +2,8 @@ public class Demo {
     public static void main(String[] args) {
         System.out.println("Hello Github!");
         System.out.println("Hello again");
+
+        // --------------------------
+        System.out.println(1 == 1);
     }
 }
